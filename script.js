615f567f16235ed8d355c65460a9e3d158e932eb
Mikhail Mazurkevich <mazurkevich.mikhail.14@gmail.com>
2026-01-27 20:37:21 +0300

    Upgrade #2 code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const a = express(),
 	p = process.env.PORT || 3000
 const gcd = (x, y) => (y ? gcd(y, x % y) : x)
 const lcm = (x, y) => (x * y) / gcd(x, y)
-a.get('/mazurkevich_mikhail_14_gmail_com', (req, res) => {
+a.get('/app/mazurkevich_mikhail_14_gmail_com', (req, res) => {
 	const x = +req.query.x,
 		y = +req.query.y
 	if (!Number.isInteger(x) || !Number.isInteger(y) || x < 1 || y < 1)
